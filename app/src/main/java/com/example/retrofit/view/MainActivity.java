8281d37050d420a80ca3a2358eafc545aa5d801a
Gabriel Neri <neri.gabriel78@gmail.com>
2020-05-26 08:02:04 -0300

    more comments about retrofit

--- app/src/main/java/com/example/retrofit/view/MainActivity.java
+++ app/src/main/java/com/example/retrofit/view/MainActivity.java
@@ -16,10 +16,19 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+/*
+* Primeiramente para usar o retrofit precisamos importar duas bibliotecas no gradle,
+* retrofit
+*   implementation 'com.squareup.retrofit2:retrofit:2.9.0'
+* e o jackson (conversor e serializador para json)
+*   implementation 'com.squareup.retrofit2:converter-jackson:2.9.0'
+* */
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding bindig;
+    //Retrofit config
     RetrofitConfig retrofitConfig;
+    //'Chamada' para a requisição
     Call<Pokemon> request;
 
     @Override
@@ -28,14 +37,18 @@ public class MainActivity extends AppCompatActivity {
         this.bindig = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(this.bindig.getRoot());
 
+        //Starto meu objeto retrofit
         this.retrofitConfig = new RetrofitConfig();
 
         this.bindig.btnRequest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int id = Integer.parseInt(bindig.edtId.getText().toString());
+                //Seto o endpoit utilizado
                 request = retrofitConfig.getPokemonAPI().getPokemonById(id);
+                //Faço minha requisição
                 request.enqueue(new Callback<Pokemon>() {
+                    //Me retorna dois métodos, onresponse e onfailure
                     @Override
                     public void onResponse(Call<Pokemon> call, Response<Pokemon> response) {
                         Pokemon pokemon = response.body();
